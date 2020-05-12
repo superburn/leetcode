9281bb52cc9bce466753426d58010009b43f9747
heshibo <heshibo01@baidu.com>
2020-05-12 11:46:48 +0800

    practise

---
+++ src/easy/array/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+package easy.array;
+
+//给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+//不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1)额外空间的条件下完成。
+//示例 1:
+//
+//给定数组 nums=[1,1,2],
+//函数应该返回新的长度 2,并且原数组 nums 的前两个元素被修改为 1,2。
+//你不需要考虑数组中超出新长度后面的元素。
+public class RemoveDuplicates {
+    public int myRemoveDuplicates(int[] nums) {
+        int length = nums.length;
+
+        for (int i = 1; i < length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                for (int j = i - 1; j < length - 1; j++) {
+                    nums[j] = nums[j + 1];
+                }
+                length--;
+                i--;
+            }
+        }
+
+        return length;
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
